8ee1a1ac929191629d74dcd7daac013d1ef85df6
Oleksii-Koval <oleksii270@gmail.com>
2023-10-07 15:43:16 +0200

    add

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,28 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+
+galleryList.insertAdjacentHTML('afterbegin', markup(galleryItems));
+
+
+function markup(arr) {
+    return arr.map(({ preview, original, description }) => `
+   <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+    `).join('')
+};
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionPosition: 'bottom',
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+
+// console.log(galleryItems);
