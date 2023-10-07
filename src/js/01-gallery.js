0993ac161a22e832aef0ff91347457909ffaab3e
Oleksii-Koval <oleksii270@gmail.com>
2023-10-07 16:46:07 +0200

    test

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,6 @@
 import "simplelightbox/dist/simple-lightbox.min.css";
 import SimpleLightbox from 'simplelightbox';
+import { markupGallery } from "./helpers";
 // Add imports above this line
 
 import { galleryItems } from './gallery-items';
@@ -10,17 +11,17 @@ import { galleryItems } from './gallery-items';
 const galleryList = document.querySelector('.gallery');
 galleryList.style.listStyle = 'none'
 
-galleryList.insertAdjacentHTML('afterbegin', markup(galleryItems));
+galleryList.insertAdjacentHTML('afterbegin', markupGallery(galleryItems));
 
-function markup(arr) {
-    return arr.map(({ preview, original, description }) => `
-   <li class="gallery__item">
-   <a class="gallery__link" href="${original}">
-      <img class="gallery__image" src="${preview}" alt="${description}" />
-   </a>
-</li>
-    `).join('')
-};
+// function markupGallery(arr) {
+//     return arr.map(({ preview, original, description }) => `
+//    <li class="gallery__item">
+//    <a class="gallery__link" href="${original}">
+//       <img class="gallery__image" src="${preview}" alt="${description}" />
+//    </a>
+// </li>
+//     `).join('')
+// };
 
 const lightbox = new SimpleLightbox('.gallery a', {
   captionPosition: 'bottom',
---
+++ src/js/helpers.js
@@ -0,0 +1,9 @@
+export function markupGallery(arr) {
+    return arr.map(({ preview, original, description }) => `
+   <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+    `).join('')
+};
